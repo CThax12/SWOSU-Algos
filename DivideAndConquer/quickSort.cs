9beca17553d086f1734b1369124d10317779b515
Cthax12 <Cthax12@gmail.com>
2023-01-19 20:55:09 -0600

    added quick sort method

--- DivideAndConquer/quickSort.cs
+++ DivideAndConquer/quickSort.cs
@@ -1,3 +1,40 @@
 class QuickSort {
-    
+    static int Partition(int[] numsToSort, int low, int high) {
+
+        //The first step of quick sort is to pick a pivot
+
+        int pivot = numsToSort[high];
+        int lowIndex = (low - 1);
+
+        //checks if number is less than pivot, if so reassigns accordingly
+
+        for (int i = low; i < high; i++) {
+            
+            if (numsToSort[i] <= pivot) {
+                lowIndex++;
+
+                int tempLow = numsToSort[lowIndex];
+                numsToSort[lowIndex] = numsToSort[i];
+                numsToSort[i] = tempLow;
+            }
+        }
+
+        int temp1 = numsToSort[lowIndex + 1];
+        numsToSort[lowIndex + 1] = numsToSort[high];
+        numsToSort[high] = temp1;
+
+        return lowIndex + 1;
+
+    }
+
+    static void QuickSort(int[] numsToSort, int low, int high) {
+        if (low < high) {
+            int partitionIndex = Partition(numsToSort, low, high);
+
+            QuickSort(numsToSort, low, partitionIndex - 1);
+            QuickSort(numsToSort, partitionIndex + 1, high);
+        }
+    }
+
+
 }
